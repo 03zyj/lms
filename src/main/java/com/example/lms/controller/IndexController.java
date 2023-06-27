67fa502f8afaf58a3b1ab6a8099ca67036ccb52d
03zyj <1058479321@qq.com>
2023-06-27 20:54:44 +0800

    基本框架

--- src/main/java/com/example/lms/controller/IndexController.java
+++ src/main/java/com/example/lms/controller/IndexController.java
@@ -1,12 +1,22 @@
 package com.example.lms.controller;
 
+import com.example.lms.bean.Courses;
+import com.example.lms.service.CoursesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
+    @Autowired
+    CoursesService coursesService;
     @GetMapping("/")
-    String index() {
+    String index(Model model) {
+        List<Courses> coursesList = coursesService.findAll();
+        model.addAttribute("coursesList", coursesList);
         return "index";
     }
 }
